5d7c0056d8023bb15889bb4fdac9a0211b05c0b6
Tanya Saroha <tanyasaroha93@gmail.com>
2023-02-17 00:00:00 +0530

    Added association for category and video

--- db/migrate/20230216182059_create_videos.rb
+++ db/migrate/20230216182059_create_videos.rb
@@ -3,9 +3,6 @@ class CreateVideos < ActiveRecord::Migration[6.1]
     create_table :videos do |t|
 
       t.string :title
-      t.string :thumbnail_64
-      t.string :thumbnail_128
-      t.string :thumbnail_264
       t.string :file
 
       t.references :category, index: true
